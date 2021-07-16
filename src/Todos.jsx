ba19cece0b833a15e97a6c643ef8c03ebec52311
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-16 15:51:16 +0200

    fetch todos based on sorting buttons

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -17,12 +17,16 @@ export default function Todos({ error, loading, setLoading, setError }) {
 
   const [todos, setTodos] = useState([]);
   const [currId, setCurrId] = useState(null); // Used to show loading spinner in specific todo on delete
-  const [sortOrder, setSortOrder] = useState('desc'); // value and trigger for sorting the todos
+  const [order, setOrder] = useState('desc'); // value and trigger for sorting the todos
+  const [sort, setSort] = useState();
 
   useEffect(() => {
+    // if sort is set, append to url
+    const sortParameter = sort ? `&status=${sort}` : ""
+    
     async function getTodos() {
       try {
-        const response = await fetch(`${baseUrl}/todos?_sort=added&_order=${sortOrder}`);
+        const response = await fetch(`${baseUrl}/todos?_sort=added&_order=${order}${sortParameter}`);
         if (response.ok) {
           const json = await response.json();
           setTodos(json);
@@ -36,7 +40,7 @@ export default function Todos({ error, loading, setLoading, setError }) {
       }
     }
     getTodos();
-  }, [loading, sortOrder]); // anyting listed in the dependency array can trigger a re-run of useEffetct
+  }, [loading, order]); // anyting listed in the dependency array can trigger a re-run of useEffetct
 
   async function doneHandler(e) {
     setCurrId(e.target.id);
@@ -84,9 +88,19 @@ export default function Todos({ error, loading, setLoading, setError }) {
   }
 
   function sortHandler(e) {
+    
+    //ta e.target.id och plockat ut strängen efter status-
+    // hämta todos baserat på parametern
+    const parameter = e.target.id.substr(7).toUpperCase() 
+
+    setSort(parameter);
     setLoading(true);
 
-    setSortOrder(e.target.id);
+  }
+  function orderHandler(e) {
+    setLoading(true);
+
+    setOrder(e.target.id);
   }
 
   if (todos.length === 0) return <h1>You don't have any todos</h1>;
@@ -98,33 +112,34 @@ export default function Todos({ error, loading, setLoading, setError }) {
       </div>
       <div className="pa4">
         <h2>Tasks</h2>
-        <a id="status-all" className="w-auto f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
-          <span className="pl1">All</span>
-        </a>
-        <a id="status-active" className="w-auto f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
-          <span className="pl1">Active</span>
-        </a>
-        <a id="status-completed" className="w-auto f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
-          <span className="pl1">Completed</span>
-        </a>
+        <button id="status-all" onClick={sortHandler} className="w-auto f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
+          All
+        </button>
+        <button id="status-active" onClick={sortHandler} className="w-auto f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
+          Active
+        </button>
+        <button id="status-done" onClick={sortHandler} className="w-auto f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
+          Completed
+        </button>
       </div>
       <div>
         <a
           id="desc"
           className="pointer w-auto f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4"
-          onClick={sortHandler}
+          onClick={orderHandler}
         >
           &uarr;
         </a>
         <a
           id="asc"
           className="pointe w-auto f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4r"
-          onClick={sortHandler}
+          onClick={orderHandler}
         >
           &darr;
         </a>
 
-        {todos.map((todo, i) => (
+        {
+          todos.map((todo, i) => (
           <Todo
             currId={currId}
             key={todo.id}
